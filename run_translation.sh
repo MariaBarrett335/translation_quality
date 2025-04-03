c2d88bb2fc12a2c755cce66676a10e4538bc5d54
Maria Barrett <maria.barrett@amd.com>
2025-04-03 11:13:34 +0300

    A script for translation one or multiple colums containing str or list of strs

--- run_translation.sh
+++ run_translation.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks-per-node=1     # 8 MPI ranks per node, 128 total (16x8)
 #SBATCH --gpus-per-node=4
 #SBATCH --mem=256G 
-#SBATCH --time=08:00:00       # Run time (d-hh:mm:ss) # 4 hours for everything
+#SBATCH --time=02:00:00       # Run time (d-hh:mm:ss) # 4 hours for everything
 #SBATCH --account=project_462000615  # Project for billing
 
 module use /appl/local/csc/modulefiles/
@@ -24,18 +24,27 @@ source .venv/bin/activate
 
 mkdir -p .logs
 
+os.environ["OPENAI_API_KEY"]=xxx
+
+os.environ["HF_HOME"]="/scratch/project_462000354/cache"
 # Define the arguments
-SOURCE_FILE="/scratch/project_462000353/maribarr/FastChat/fastchat/llm_judge/data/mt_bench/question.jsonl"
-SOURCE_FILE="hf://datasets/CohereForAI/m-ArenaHard/en/test-00000-of-00001.parquet"
-BENCH_NAME="mt_bench"
-BENCH_NAME='arenahard'
-TRANSLATE_COL='turns'
-TRANSLATE_COL='prompt'
+#SOURCE_FILE="/scratch/project_462000353/maribarr/FastChat/fastchat/llm_judge/data/mt_bench/question.jsonl"
+#BENCH_NAME="mt_bench"
+#TRANSLATE_COL='turns'
+
+#SOURCE_FILE="hf://datasets/CohereForAI/m-ArenaHard/en/test-00000-of-00001.parquet"
+#BENCH_NAME='arenahard'
+#TRANSLATE_COL='prompt'
+
+SOURCE_FILE="hf://datasets/basicv8vc/SimpleQA/simple_qa_test_set.csv"
+BENCH_NAME='simpleqa'
+TRANSLATE_COL='problem'
+TRANSLATE_COL2='answer' 
 
 OUTPUT_DIR="translations/${BENCH_NAME}"
 mkdir -p $OUTPUT_DIR
 LANGUAGES=("da" "de" "fi" "el" "fr" "pl")
-
+LANGUAGES=("pl")
 export SSL_CERT_FILE="" # litellm does not run with this environment variable set. The value was /etc/ssl/ca-bundle.pem I restore it immediately after running the script
 
 # Iterate over each language and run the translation script
--- translation_scripts/translate_list_str.py
+++ translation_scripts/translate_list_str.py
@@ -8,6 +8,7 @@ import math
 import time
 import sys
 from litellm import completion
+from iso639 import Lang
 
 tqdm.pandas() 
 
@@ -20,6 +21,28 @@ It checks whether an Opus model exist for the English -> target language and if 
 use NLLB instead. Or you can set the --deepl or gemini flag and use DeepL/Gemini for translation.
 Author: Maria
 """
+
+def create_translation_prompt(source_sentence:str, target_lang:str, source_lang='English') -> str:
+    prompt = f"""Translate the following {source_lang} sentence into a faithful and fluent {target_lang} translation. Do not include the source sentence in the response. Do not say anything else than the translation""
+    Source sentence: {source_sentence}
+    """
+    return prompt
+
+def call_litellm(prompt, model="gpt-4o", max_tokens=1000, temperature=0.0):
+    try:
+        response = completion(
+            model=model,
+            messages = [
+                {"role": "system", "content": "You are a helpful text edit and translation assistant."},
+                { "content": prompt, "role": "user"}
+                ],
+            stream=False,
+            max_completion_tokens=max_tokens,
+            temperature=temperature
+        )
+        return response
+    except Exception as e:
+        sys.exit(e)
 
 def opus_model_exists(model_name: str) -> bool:
     try:
@@ -104,52 +127,74 @@ if __name__ == "__main__":
     parser.add_argument('--source-file', type=str, required=False, default='hf://datasets/CohereForAI/m-ArenaHard/en/test-00000-of-00001.parquet', help="Path to the English source file")
     parser.add_argument('--output-dir', type=str, required=True, help="Path to the output file")
     parser.add_argument('--max-samples', type=int, default=None, help="Only take top n rows")
-    parser.add_argument('--translate-col', type=str, default='prompt', help='The name of the column to translate')
+    parser.add_argument('--translate-col', nargs='+', help='The name of the column to translate', required=True)
     args = parser.parse_args()
+
+
+    print("Translating colums", args.translate_col)
     
     src_lang = 'en'
+    src_lang_name = Lang(src_lang).name
+    tgt_lang_name = Lang(args.tgt_lang).name
+
     # Load the model and tokenizer
     model_name = f'Helsinki-NLP/opus-mt-{src_lang}-{args.tgt_lang}'
 
     output_file = os.path.join(args.output_dir, f"{args.tgt_lang}.jsonl")
 
-    print(f"Translating MT bench questions from en to {args.tgt_lang}")
+    print(f"Translating MT bench questions from {src_lang_name} to {tgt_lang_name}")
     print(f"Reading en questions from {args.source_file}")
 
     # Check if the output directory exists, if not create it
     if not os.path.exists(args.output_dir):
         os.makedirs(args.output_dir)
-
-    # check if the opus model exists
-    if opus_model_exists(model_name):
-        print("Opus model found")
-        tokenizer = MarianTokenizer.from_pretrained(model_name)
-        model = MarianMTModel.from_pretrained(model_name)
-    else:
-        print('Opus model not found, Exiting')
-        sys.exit(1)
         
     #open the English source file depending on the format
     if args.source_file.split('.')[-1] == 'parquet': 
         df = pd.read_parquet(args.source_file)
     elif args.source_file.split('.')[-1] == 'jsonl': 
         df = pd.read_json(args.source_file, lines=True)
+    elif args.source_file.split('.')[-1] == 'csv':
+        df = pd.read_csv(args.source_file, sep=',')
+    elif args.source_file.split('.')[-1] == 'tsv':
+        df = pd.read_csv(args.source_file, sep='\t')
     else:
-        print('Currently only supporting jsonl and parquet')
+        print('Currently only supporting jsonl, tsv, csv, tsv and parquet')
 
     if args.max_samples:
         df = df.head(args.max_samples)
 
+    # check if the opus model exists
+    if opus_model_exists(model_name):
+        print("Opus model found")
+        using_opus = True
+    else:
+        print('Translating using GPT-4o')
+        using_opus = False
+
     # Translate using Opus, translate each list item in case it is a list
-    df.loc[:, f'{args.translate_col}_mt-translated'] = df[args.translate_col].progress_map(
-    lambda x: [translate_opus(item, tokenizer=tokenizer, model=model) for item in x] 
-             if isinstance(x, list) 
-             else translate_opus(x, tokenizer=tokenizer, model=model)
-    )
-                                                            
-    # save to file
+    if using_opus:
+        tokenizer = MarianTokenizer.from_pretrained(model_name)
+        model = MarianMTModel.from_pretrained(model_name)
+        for col in args.translate_col:
+            df.loc[:, f'{col}_mt-translated'] = df[col].progress_map(
+            lambda x: [translate_opus(item, tokenizer=tokenizer, model=model) for item in x] 
+                    if isinstance(x, list) 
+                    else translate_opus(x, tokenizer=tokenizer, model=model)
+            )
+
+    else:
+        for col in args.translate_col:
+            df.loc[:, f'{col}_mt-translated'] = df[col].progress_map(
+            lambda x: [call_litellm(create_translation_prompt(source_sentence=item, target_lang=src_lang_name)) for item in x] 
+                    if isinstance(x, list) 
+                    else call_litellm(create_translation_prompt(source_sentence=x, target_lang=tgt_lang_name))
+        )
+
+                                                      
     #rename the english question column
-    df.rename(columns={args.translate_col: f'{args.translate_col}_{src_lang}'}, inplace=True)
+    df.rename(columns={col: f'{col}_{src_lang}'}, inplace=True)
+    
     df.to_json(output_file, orient='records', lines=True, force_ascii=False)
 
     print(f'Wrote to file {output_file}')
